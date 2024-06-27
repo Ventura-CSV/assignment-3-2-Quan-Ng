c903a9958d5dfb2ef9f6031b9a947868a6324dcd
Quan-Ng <QNguyen7#07@gmail.com>
2024-06-26 18:36:45 -0700

    ASSIG: half done

--- main.py
+++ main.py
@@ -7,7 +7,32 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    if (num1 < num2):
+        if (num1 < num3): 
+            minval = num1
+        else:
+            minval = num3
+            
+    else:
+        if (num2 < num3):
+            minval = num2
+        else:
+            minval = num3
+    
+    if (num1 > num2):
+        if (num1> num3):
+            maxval = num1
+        else:
+            maxval = num3
+    else:
+        if (num2 > num3):
+            maxval = num2
+        else:
+            maxval = num3
+            
+    if (
+        
+          
     print(minval, median, maxval)
     ########################################
     # Do not delete the return statement
